e6efdc2229263b970a3650ffaaff7879557f3e88
sampathbalsu2 <sampath.balsu@gmail.com>
2024-12-15 07:42:22 +0530

    FOREST

---
+++ tables.tf
@@ -0,0 +1,22 @@
+resource "google_bigquery_table" "default" {
+  dataset_id          = "sampath"
+  table_id            = "mytable"
+  deletion_protection = false # set to "true" in production
+
+  schema = <<EOF
+[
+  {
+    "name": "ID",
+    "type": "INT64",
+    "mode": "NULLABLE",
+    "description": "Item ID"
+  },
+  {
+    "name": "Item",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+
+}
